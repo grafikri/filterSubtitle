9ebea1c37c1e8f72fdd6a1c2989a84176079a939
Serhan Coşkun <grafikri@gmail.com>
2021-05-12 19:20:42 +0300

    add new files

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const fs = require('fs')
 const allWords = require('./words')
 try {
 
-  
   const data = fs.readFileSync('./subtitle.srt', 'utf8')
 
-  const myWords = allWords.filter(item => data.indexOf(item) !== -1)
+  const myWords = allWords.filter(item => data.indexOf(item) !== -1).map(item => item + '\n')
 
+  const myWordsText = myWords.reduce((prev,current) => {
+    prev = prev + current
+    return prev
+  }, '')
   
-
-  console.log('words: ', myWords.length);
+  fs.writeFileSync('helloworld.txt', myWordsText);
+  console.log('count: ', myWords.length);
 
 
 } catch (err) {
